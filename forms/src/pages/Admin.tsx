8e4e9a17b1989d340fdaa2feffb9ee380a7370e9
happy436 <lelouch436@gmail.com>
2024-06-12 18:28:04 +0300

    1

--- forms/src/pages/Admin.tsx
+++ forms/src/pages/Admin.tsx
@@ -1,13 +1,15 @@
-import { Badge, Card, Divider } from "@tremor/react";
+import { Badge, Button, Card, Divider } from "@tremor/react";
 import React, { useState } from "react";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import Column from "./Column";
 
 type AdminProps = {};
 
 const Admin: React.FC<AdminProps> = () => {
-	const listTaskTypes = ["Двигун", "Клапан", "Робота з архівом"];
+	const listTaskTypes = [
+		"Двигун",
+		"Клапан",
+		"Робота з архівом",
+		"Уборка мусора",
+	];
 	const mockData = [
 		{
 			id: "adad",
@@ -21,33 +23,38 @@ const Admin: React.FC<AdminProps> = () => {
 		},
 	];
 	const [data, setData] = useState(mockData);
-	const [leftWords, setLeftWords] = useState(["Word1", "Word2", "Word3"]);
-	const [rightWords, setRightWords] = useState(["Word4", "Word5", "Word6"]);
-	const initialLeftWords = ["Word1", "Word2", "Word3"];
-	const initialRightWords = ["Word4", "Word5", "Word6"];
+	const [newData, setnewData] = useState([]);
+	const [name, setName] = useState("");
+
+	const handleMouseEnter = (name) => {
+		let newData = data.find((item) => item.name === name)?.listCategories;
+		newData = listTaskTypes.filter((item) => !newData.includes(item));
+		setnewData(newData);
+		setName(name);
+	};
+
+	const handleMouseLeave = () => {
+		setnewData([]);
+		setName("");
+	};
+
+	const handleAddCategory = (name, category) => {
+		setData((prevData) =>
+			prevData.map((item) => {
+				if (item.name === name) {
+					return {
+						...item,
+						listCategories: [...item.listCategories, category],
+					};
+				}
+				return item;
+			})
+		);
+        handleMouseLeave()
+	};
 
 	return (
 		<div>
-			<DndProvider backend={HTML5Backend}>
-				<div className="flex space-x-4">
-					<Column
-						words={leftWords}
-						setWords={setLeftWords}
-						oppositeWords={rightWords}
-						setOppositeWords={setRightWords}
-						initialWords={initialLeftWords}
-						isLeftColumn={true}
-					/>
-					<Column
-						words={rightWords}
-						setWords={setRightWords}
-						oppositeWords={leftWords}
-						setOppositeWords={setLeftWords}
-						initialWords={initialRightWords}
-						isLeftColumn={false}
-					/>
-				</div>
-			</DndProvider>
 			<h1 className="mb-3">HR panel</h1>
 			<div className="flex gap-3 flex-col">
 				<Card className="w-max">
@@ -64,23 +71,46 @@ const Admin: React.FC<AdminProps> = () => {
 									<span className="flex-grow basis-1/3">
 										{worker.name}
 									</span>
-									<Card className="flex-grow basis-2/3">
+									<Card
+										className="flex-grow basis-2/3"
+										onMouseEnter={() =>
+											handleMouseEnter(worker.name)
+										}
+										onMouseLeave={handleMouseLeave}
+									>
 										<h2 className="text-xl mb-3">
 											Категории работ
 										</h2>
-										<DndProvider backend={HTML5Backend}>
-											<ul className="flex flex-wrap gap-3">
-												{worker.listCategories.map(
-													(item) => (
-														<li key={item}>
-															<Badge>
-																{item}
-															</Badge>
-														</li>
-													)
-												)}
-											</ul>
-										</DndProvider>
+                                        <p className="text-slate-600">наведи на меня мышкой</p>
+										<ul className="flex flex-wrap gap-3">
+											{worker.listCategories.map(
+												(item) => (
+													<li key={item}>
+														<Badge>{item}</Badge>
+													</li>
+												)
+											)}
+											{name === worker.name &&
+												newData.map((item) => (
+													<li key={item}>
+														<Badge color="gray">
+															<Button
+																className="m-0 mr-2 p-0"
+																color="gray"
+																onClick={() =>
+																	handleAddCategory(
+																		worker.name,
+																		item
+																	)
+																}
+															>
+																+
+															</Button>
+															{item}
+														</Badge>
+													</li>
+												))}
+										</ul>
 									</Card>
 								</div>
 								<Divider />
